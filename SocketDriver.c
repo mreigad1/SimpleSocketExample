287c1cbc3424c141d9a74c7bd7fbb0a8e0cd19ac
matt reigada <matt.reigada@raymondcorp.com>
2018-03-27 15:54:17 -0400

    mild refactor for server port

--- SocketDriver.c
+++ SocketDriver.c
@@ -7,13 +7,15 @@
 #include "SocketDriver.h"
 #include "debug.h"
 
+#define SERVER_PORT_NO 80
+
 static const int g_domain = AF_INET;
 static const int g_type = SOCK_STREAM;
 static const int g_protocol = 0;
 static const int g_bufferSize = 4096;
 
 bool getSocket(int* const fd);
-bool socketBind(SocketDriver* const socketDat);
+bool socketBind(SocketDriver* const s);
 
 // @Procedure - opens socket and provides file descriptor
 // @return - true if success, false if failed
@@ -36,40 +38,38 @@ bool getSocket(int* const fd) {
 // @Procedure - opens and binds socket, provides file
 //              descriptor and bind result
 // @return - true if success, false if failed
-// @param[socketDat] - socket data structure to bind results to
-bool socketBind(SocketDriver* const socketDat) {
+// @param[s] - socket data structure to bind results to
+bool socketBind(SocketDriver* const s) {
 
-	ASSERT(socketDat);											//check for valid param
+	ASSERT(s);													//check for valid param
 	bool rv = true;												//default true, false by ERROR_IF
 	int socketResult = -1; 										//default socket/stream returns invalid
 	int bindResult = -1;
 	ERROR_IF (false == getSocket(&socketResult));				//error if socket not acquired
-	struct sockaddr* sockAddInfo = malloc(sizeof(sockaddr_in));	//allocate sockaddr info and assign data to instance
-	ASSERT(NULL != sockAddInfo);								//check that buffer was successfully acquired
-	(*(sockaddr_in*)sockAddInfo) = {							//assign values of struct
-		g_domain,
-		htons(0),
-		{
-			htonl(INADDR_ANY)
-		},{
-			0
-		}
-	};
+	struct sockaddr_in* sockInfo = malloc(sizeof(sockaddr_in));	//allocate sockaddr info and assign data to instance
+	ASSERT(NULL != sockInfo);									//check that buffer was successfully acquired
+	sockInfo->sin_family = g_domain;							//assign values
+	sockInfo->sin_addr.s_addr = htonl(INADDR_ANY);
+	#ifdef IS_SERVER
+		sockInfo->sin_port = SERVER_PORT_NO;
+	#else
+		sockInfo->sin_port = htons(0);
+	#endif
 
 	bindResult = bind(											//attempt socket binding
 		socketResult,
-		sockAddInfo,
-		sizeof(sockAddInfo)
+		sockInfo,
+		sizeof(sockInfo)
 	);	
 	ERROR_IF (bindResult < 0);									//error if socket fails binding	
-	(*socketDat) = { socketResult, bindResult, sockAddInfo };	//reaching this point means success,
+	(*s) = { socketResult, bindResult, (sockaddr*)sockInfo };	//reaching this point means success,
 																//return intermediate values to input struct
 
 	doReturn:
 		
 		if (false == rv) {										//if error
-			if (socketResult >= 0)   { close(socketResult); }	//if socket opened but binding failed then close socket
-			if (NULL != sockAddInfo) { free(sockAddInfo); }		//clean up allocated struct on error
+			if (socketResult >= 0) { close(socketResult); }		//if socket opened but binding failed then close socket
+			if (NULL != sockInfo)  { free(sockInfo); }			//clean up allocated struct on error
 		}
 		return rv;
 }
@@ -87,6 +87,8 @@ void closeSocketDriver(SocketDriver* const s) {
 }
 
 bool clientConnect(SocketDriver* const s, int ipComponents[4]) {
+	ASSERT(s);														//verify inputs valid
+	ASSERT(ipComponents);
 	bool rv = true;													//assume success
 	const int numIPComponents = 4;									//number of components in IPv4
 	char* sin_addr = &(((sockaddr_in*)s->socketData)->sin_addr);	//take address of buffer for memcpy
@@ -105,5 +107,6 @@ bool clientConnect(SocketDriver* const s, int ipComponents[4]) {
 }
 
 void serverConnect(SocketDriver* const s) {
-	
+	ASSERT(s);														//verify input valid
+
 }
