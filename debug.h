7a3978f60b62e134cb04216a1beacd8e7b75cbd7
matt reigada <matt.reigada@raymondcorp.com>
2018-03-30 20:52:21 -0400

    disabled line logging macro

--- debug.h
+++ debug.h
@@ -24,6 +24,6 @@
 #define ERROR_IF(X)	ERROR_IMPL( X,			rv = false,	goto doReturn	)
 #define ASSERT(X)	ERROR_IMPL( false==(X),	NULL,		exit(0)			)
 
-#define LINE_LOG do { printf("FILE(%s):FUNC(%s):L(%d)\n", __FILE__, __FUNCTION__, __LINE__); fflush(stdout); } while(0)
-//#define LINE_LOG do { ; } while(0)
+//#define LINE_LOG do { printf("FILE(%s):FUNC(%s):L(%d)\n", __FILE__, __FUNCTION__, __LINE__); fflush(stdout); } while(0)
+#define LINE_LOG do { ; } while(0)
 
